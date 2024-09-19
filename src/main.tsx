71322e5ef26ad0ae673cc756d22a9813b9a7c727
Giovanni Remigi <gremigi@theaiinstitute.com>
2024-09-19 15:32:01 +0200

    Revert "Auxiliary commit to revert individual files from a29bf03e72bf1d5e0544698296af3080cf099e51"
    This reverts commit 27dcb74a331fbb1f1fc3b2eb5cc8cf12cb471bb6.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
 
+import "./css/bulma.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
